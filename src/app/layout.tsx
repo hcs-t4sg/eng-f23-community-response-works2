1a7d14e07b2616441a11930b62ffccbf69b76457
MichaelCai0301 <mcai@college.harvard.edu>
2023-12-29 21:52:34 -0500

    cleaned up layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from './dataDashboard/components/navbar'
 import Footer from './dataDashboard/components/footer'
-import Test from './Test'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
